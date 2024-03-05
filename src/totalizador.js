8a25b2100c6d083d423aa9fd12a41ae20c48178f
Patrick <patrickgonzalesquintana@gmail.com>
2024-03-05 14:19:56 -0400

    Se factorizo los impuestos con calcular impuestos por separado

--- src/totalizador.js
+++ src/totalizador.js
@@ -2,19 +2,23 @@ function precioNeto(cantItems, precio){
     return cantItems * precio;
 }
 
-function calcularTotal(neto, estado){
-    let res; 
+function calcularImpuesto(neto, estado){
+    let impuesto;
     if (estado == "CA")
-        res = neto + (neto * 0.0825);
+        impuesto = neto * 0.0825;
     if (estado == "AL")
-        res = neto + (neto * 0.04);
+        impuesto = neto * 0.04;
     if (estado == "TX")
-        res = neto + (neto * 0.0625);
+        impuesto = neto * 0.0625;
     if (estado == "NV")
-        res = neto + (neto * 0.08);
+        impuesto = neto * 0.08;
     if (estado == "UT")
-        res = neto + (neto * 0.0665);
-    return res;
+        impuesto = neto * 0.0665;
+    return impuesto;
+}
+
+function calcularTotal(neto, estado){
+    return neto + calcularImpuesto(neto, estado);
 }
 
 export {precioNeto, calcularTotal};
