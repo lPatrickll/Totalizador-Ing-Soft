a60735b5150ea4e25b94110d677defb533171d6e
Patrick <patrickgonzalesquintana@gmail.com>
2024-03-05 13:05:00 -0400

    Se termino la funcion precio neto

--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -2,8 +2,12 @@ describe("Totalizador", () => {
     it("deberia calcular precio neto de 3 items a precio de 2 = 6", () => {
         expect(precioNeto(3, 2)).toEqual(6);
     });
+
+    it("deberia calcular precio neto de 5 items a precio de 2 = 10", () => {
+        expect(precioNeto(5, 2)).toEqual(10);
+    });
 });
 
 function precioNeto(cantItems, precio){
-    return 6;
+    return cantItems * precio;
 }
