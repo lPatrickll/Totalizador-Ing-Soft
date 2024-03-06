201a1b1d4513d72d261f342f559d84a6c76f5c05
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-03-05 22:51:18 -0400

    Se implemento la interfaz para el Descuento de costo de envio de acuerdo al tipo de Cliente

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import {precioNeto, calcularTotal, calcularImpuesto, porcentajeImpuesto, calcularDescuento, porcentajeDecuento, porcentajeCategoriaImpuesto, categoriaImpuesto, categoriaDescuento, porcentajeCategoriaDescuento, costoEnvio, calcularPeso} from "./totalizador";
+import {precioNeto, calcularTotal, calcularImpuesto, porcentajeImpuesto, calcularDescuento, porcentajeDecuento, porcentajeCategoriaImpuesto, categoriaImpuesto, categoriaDescuento, porcentajeCategoriaDescuento, costoEnvio, calcularPeso, calcularDescuentoTipoCliente,calcularDescuentoCostoEnvioCliente} from "./totalizador";
 
 const cantItems = document.querySelector("#cantidad-items");
 const precio = document.querySelector("#precio");
@@ -6,6 +6,7 @@ const form = document.querySelector("#totalizador-form");
 const estado = document.querySelector("#codigoEstado");
 const categoria = document.querySelector("#categoria");
 const peso = document.querySelector("#peso");
+const cliente = document.querySelector("#cliente");
 const div = document.querySelector("#resultado-div");
 
 
@@ -17,6 +18,7 @@ form.addEventListener("submit", (event) => {
   const estadoValue = estado.value;
   const categoriaValue = categoria.value;
   const pesoNumber = Number.parseInt(peso.value);
+  const clienteValue = cliente.value;
 
   div.innerHTML = ("<p>" + "Precio neto (" + cantItemsNumber + "*$" + precioNumber + "): $" + precioNeto(cantItemsNumber, precioNumber) + "</p>" +
   "<p> Descuento (" + porcentajeDecuento(cantItemsNumber) + "%): " + calcularDescuento(precioNeto(cantItemsNumber, precioNumber), cantItemsNumber) + "</p>" +
@@ -24,6 +26,7 @@ form.addEventListener("submit", (event) => {
   "<p> Impuestos por categoria (" + porcentajeCategoriaImpuesto(categoriaValue) + "%): " + categoriaImpuesto(precioNeto(cantItemsNumber, precioNumber), categoriaValue) + "</p>" +
   "<p> Descuentos por categoria (" + porcentajeCategoriaDescuento(categoriaValue) + "%): " + categoriaDescuento(precioNeto(cantItemsNumber, precioNumber), categoriaValue) + "</p>" +
   "<p> Peso volumetrico ($" + costoEnvio(pesoNumber) + "): " + calcularPeso(cantItemsNumber, pesoNumber) + "</p>" +
+  "<p> Descuento para el cliente ("+ clienteValue +") con ("+ calcularDescuentoCostoEnvioCliente(clienteValue) +"%): " + calcularDescuentoTipoCliente(precioNeto(cantItemsNumber, precioNumber), clienteValue) + "</p>" +
   "<p>" + "Precio Total: " + calcularTotal(cantItemsNumber, precioNumber, estadoValue, categoriaValue) + "</p>" 
   );
 
